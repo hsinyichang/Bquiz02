03334588db020b9aa59d6276cdccf00d59e31d76
gina <f1233772002@gmail.com>
2022-07-29 14:39:18 +0800

    完成文章的管理與顯示功能-註解

--- api/news.php
+++ api/news.php
@@ -2,16 +2,16 @@
 include_once "../base.php";
 
 if(isset($_POST['del'])){//若沒有勾選就不會送出
-    foreach($_POST['del'] as $id){
-        $News->del($id);
+    foreach($_POST['del'] as $id){//列出有勾選的刪除選項的id
+        $News->del($id);   //把該選項刪掉
     }
 }
 
-$rows=$News->all();
+$rows=$News->all();  //列出全部的資料  主要是sh
 foreach($rows as $row){
-    if(in_array($row['id'],$_POST['sh'])){
-        $row['sh']=1;
-    }else{
+    if(in_array($row['id'],$_POST['sh'])){  //確定修改送出後  有在陣列裡的
+        $row['sh']=1;    
+    }else{     //沒在陣列裡的  (沒有勾選sh)
         $row['sh']=0;
     }
     $News->save($row);
